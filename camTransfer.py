c3189fdf92d70cdef6d600fdaca7b3acbbe156cc
Joe <jwhueh@gmail.com>
2016-06-30 02:32:04 -0700

    jwh

--- camTransfer.py
+++ camTransfer.py
@@ -30,31 +30,25 @@ class WebTransfer(object):
 				if re.search('jpg', line):
 					"""self.log(("found: " + str(line) +'\n'))
 					l = line.split('/')
-<<<<<<< HEAD
 					t = l[2].lstrip('se_').lstrip('s_').rstrip('.jpg\n')
 					t = time.strptime(t, "%Y%m%d_%H%M%S_%f")
 					t = time.mktime(t)
 					print t, time.time(), l
 					if (time.time()- t) < 4000:"""
 					self.upload(line.rstrip('\n'))
-=======
 					#t = l[2].lstrip('se_').rstrip('.jpg\n')
 					#t = time.strptime(t, "%Y%m%d_%H%M%S_%f")
 					#t = time.mktime(t)
 					#if (time.time()- t) < 120:
 					self.upload(line)
->>>>>>> 6b48605a715fadcb77dc1e7d59aa98436aae8c0d
 					
                         if not line:
                                 break
 
 	def upload(self,line):
-<<<<<<< HEAD
 		l = line.split('/')
 		name = l[(len(l)-1)].split('_')[0]+".jpg"
-=======
 		print line
->>>>>>> 6b48605a715fadcb77dc1e7d59aa98436aae8c0d
 		cmd = os.path.join(self.dir, line.rstrip('\n'))
 		self.log(("uploading: " + str(cmd)))
 		print cmd, name
