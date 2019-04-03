79d0b40bfe5b96093833f20050d8a27c37e668c5
Oliver Fraser <ojf@uw.edu>
2019-04-03 15:03:24 -0700

    Rename methods and add paramiko

--- webcam_handler.py
+++ webcam_handler.py
@@ -4,17 +4,16 @@ Naming convention assumes images come in slower than once per second.
 
 Original by Matt Armstrong (~'17-'18), modified for direct access to webcams by OJF '19
 
-replace 
-  Subprocess(['rsync -azrh --progress *jpg mrouser@ovid.u.washington.edu:public_html/webcams/' with 
-  curl -T [image] scp://ovid.u.washington.edu/public_html/webcams/[name].jpg -u mrouser
-
-(found that PycURL (default package) works well, but if I run into problems, try requests (HTTP for Humans)
+Modify post_images for using paramiko to send images over ssh
+- pip install paramiko
+- how to use certificates?
 
 Exceptions in objects should pass up their problem, not print...
 """
 
 import os, shutil, subprocess, glob, time, sys
-import pycurl
+import pycurl # retrieve
+# import paramiko # sftp
 from datetime import datetime
 
 class WebCam:
@@ -28,7 +27,7 @@ class WebCam:
         self.lastImage = None
         
 
-    def save_image(self, savePath):
+    def retrieve_image(self, savePath):
         """ Uses http://IPAddress/image/jpeg.cgi to request current image, returns full path to new image. """
 
         imagePath = savePath + "/" + self.name + "_" + datetime.now().strftime("%m%d_%H%M%S") + '.jpg'
@@ -60,8 +59,10 @@ class WebCamHandler(object):
         
         self.archivePath = str(os.getcwd())+"/Archive/"
         self.archivePath = "/Users/ojf/Downloads/"
-        self.remotePath = 'ovid:public_html/webcams/'
+        self.remoteHost = 'ovid.u.washington.edu'
+        self.remotePath = 'public_html/webcams/'
         self.remotePath = './'
+        self.remotePort = 22
 
         self.cameras = []  # list of all WebCam objects
         FILE = open(webcam_definition_file)
@@ -73,7 +74,7 @@ class WebCamHandler(object):
             print "Setting up camera", camera.name
 
 
-    def retrieve_images(self):
+    def retrieve_and_archive_images(self):
         """ Check for correct daily folder in archive, then save an image from each camera
         """
         path = self.archivePath + datetime.now().strftime("%Y%m%d")
@@ -85,16 +86,23 @@ class WebCamHandler(object):
                 print "Error creating ", path, ": ", e
 
         for camera in self.cameras:
-            camera.save_image(path)
+            camera.retrieve_image(path)
             print "image archived from ", camera.name
 
 
     def post_images(self):
-        """ Push image to remote server. """
+        """ Push images to remote server. """
+        transport = paramiko.Transport((self.remoteHost, self.remotePort))
+        transport.connect(username = 'mrouser', password = 'squirrel5tar')
+
+        sftp = paramiko.SFTPClient.from_transport(transport)
         for camera in self.cameras:
             if camera.lastImage:
-                shutil.copy(camera.lastImage, self.remotePath + camera.name + ".jpg")
+                sftp.put(camera.lastImage, self.remotePath + camera.name + ".jpg")
                 print "Posted image from", camera.name
+        sftp.close()
+        transport.close()
+
 
     
 if __name__ == "__main__":
@@ -104,7 +112,7 @@ if __name__ == "__main__":
     run = True
     while run == True:
         try:
-            wh.retrieve_images()
+            wh.retrieve_and_archive_images()
         except Exception as e:
             print e
         try:
